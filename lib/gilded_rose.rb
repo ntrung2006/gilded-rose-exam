78916757dd723ad4d65df2062f48614f605e08a0
Trung Nguyen <trung.n@workhouse.me>
2019-10-23 23:52:27 +0700

    Refactor code to fix the magic number.

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -4,33 +4,39 @@ class GildedRose
   end
 
   def update_quality
+    normal_unit = 1 # Unit normal increases | degrade
+    max_quality = 50
+    min_quality = 0
+    sell_in_10 = 10
+    sell_in_5 = 5
     @items.each do |item|
       # Item quality alway > 0 and Sulfuras, being a legendary item,
       # The quality of an item is never more than 50
       # has no sell by date nor decreases in quality
-      next if item.quality <= 0 || item.quality >= 50 || item.name == 'Sulfuras, Hand of Ragnaros'
+      next if item.quality <= min_quality || item.quality >= max_quality || item.name == 'Sulfuras, Hand of Ragnaros'
 
       if ['Aged Brie', 'Backstage passes to a TAFKAL80ETC concert'].include?(item.name)
-        item.quality += 1
+        item.quality += normal_unit
         if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-          item.quality += 1 if item.sell_in < 11
-          item.quality += 1 if item.sell_in < 6
+          item.quality += normal_unit if item.sell_in <= sell_in_10
+          item.quality += normal_unit if item.sell_in <= sell_in_5
         end
       else
-        item.quality -= 1
+        item.quality -= normal_unit
       end
 
-      item.sell_in -= 1
+      item.sell_in -= normal_unit
 
       if item.sell_in.negative? #is mean item.sell_in < 0
         if item.name == 'Aged Brie'
-          item.quality += 1
+          item.quality += normal_unit
         elsif item.name == 'Backstage passes to a TAFKAL80ETC concert'
-          item.quality = 0
+          item.quality = min_quality
         elsif item.name == 'Conjured'
-          item.quality -= 2
+          # Conjured items degrade in quality twice as fast as normal items
+          item.quality -= normal_unit*2
         else
-          item.quality -= 1
+          item.quality -= normal_unit
         end
       end
     end
