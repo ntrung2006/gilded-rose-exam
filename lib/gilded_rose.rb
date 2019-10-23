2038c47cfe050422b688264fb14981f0764493a7
Trung Nguyen <trung.n@workhouse.me>
2019-10-23 23:22:38 +0700

    Refactor gilded_rose.rb for readability and easy to understand.

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -5,47 +5,30 @@ class GildedRose
 
   def update_quality
     @items.each do |item|
-      if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
+      # Item quality alway > 0 and Sulfuras, being a legendary item,
+      # The quality of an item is never more than 50
+      # has no sell by date nor decreases in quality
+      next if item.quality <= 0 || item.quality >= 50 || item.name == 'Sulfuras, Hand of Ragnaros'
+
+      if ['Aged Brie', 'Backstage passes to a TAFKAL80ETC concert'].include?(item.name)
+        item.quality += 1
+        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
+          item.quality += 1 if item.sell_in < 11
+          item.quality += 1 if item.sell_in < 6
         end
       else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
+        item.quality -= 1
       end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
+
+      item.sell_in -= 1
+
+      if item.sell_in.negative? #is mean item.sell_in < 0
+        if item.name == 'Aged Brie'
+          item.quality += 1
+        elsif item.name == 'Backstage passes to a TAFKAL80ETC concert'
+          item.quality = 0
         else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
+          item.quality -= 1
         end
       end
     end
